68fee087f54f36d3274e79754e22740ebfa0a1e4
Will Gao <brightgz@hotmail.com>
2025-05-09 13:50:28 -0400

    added rate limit 8 hours 2 messages per IP

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -11,7 +11,7 @@ if (!yourReceivingEmail) {
 }
 
 const rateLimitMap = new Map<string, { count: number; lastRequest: number }>();
-const RATE_LIMIT_WINDOW = .75 * 60 * 1000; // .75 minutes 45 seconds
+const RATE_LIMIT_WINDOW = 8 * 60 * 60 * 1000; // 8 hours
 const MAX_REQUESTS = 2;
 
 export async function POST(req: NextRequest) {
