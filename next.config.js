8309de4eb74a4aa23a86f62088145f971811973a
Will Gao <brightgz@hotmail.com>
2025-08-05 11:19:08 -0400

    Image Caching

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ export default {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 2592000, // 30 days
     loader: 'default',
     unoptimized: false,
     domains: [],
